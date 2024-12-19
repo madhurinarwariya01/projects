7943744bcbcadfebe9afdcab3483fae5a40c3903
madhuri narwariya <narwariyamadhuri9@gmail.com>
2024-12-19 14:08:17 +0530

    Add files via upload

---
+++ PR0JECTS WORKS/JAVASCRIPT/file2.js
@@ -0,0 +1,16 @@
+function toyplay(){
+let a = document.querySelector(".toyplay");
+a.setAttribute("src" , "toy.gif");
+}
+
+
+
+
+
+
+
+  function toystop() {
+    let b= document.querySelector(".toyplay");
+b.setAttribute("src" , "toy2.jpeg");
+}
+  
---
+++ PR0JECTS WORKS/tables/script.js
@@ -0,0 +1,12 @@
+
+function table() {
+
+var a = document.getElementById("n1").value;
+//  sum = 0;
+for(let i=1 ; i<=10; i++)
+    
+      {
+        sum = a*i;
+       document.write (a ," x ",i ," = ",a*i , "<br>");
+      }
+    }
